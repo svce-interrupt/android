0ed4f3156bd72b975dc4d681733d0a6dbca1f97f
Adarsh M <lazytomatostudios@gmail.com>
2017-08-03 19:18:31 +0530

    chat test

--- app/src/main/java/com/lazytomatostudios/svceinterrupt/Chat.java
+++ app/src/main/java/com/lazytomatostudios/svceinterrupt/Chat.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
 import java.util.Map;
 
 
@@ -22,24 +25,91 @@ import com.google.gson.JsonElement;
  * A simple {@link Fragment} subclass.
  */
 
-public class Chat extends Fragment implements MyInterface {
+public class Chat extends Fragment implements MyInterface, AIListener {
+
+    Button listenButton;
+    TextView resultTextView;
+
+    AIService aiService;
 
 
     public Chat() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d("Debug", "TESTING");
+
+        final AIConfiguration aiConfig = new AIConfiguration("324d16eb787f4550a42a8c728b1dd3e1",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(getContext(), aiConfig);
+        aiService.setListener(this);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        listenButton = view.findViewById(R.id.button);
+        resultTextView = view.findViewById(R.id.textView);
+
+        listenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aiService.startListening();
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void fragmentNowVisible() {
         Log.d("Debug", "Chat Screen visible");
+
     }
+
+    public void onResult(final AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        resultTextView.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString +
+                "\nReply: " + result.getFulfillment().getSpeech());
+    }
+
+    @Override
+    public void onError(final AIError error) {
+        resultTextView.setText(error.toString());
+    }
+
+    @Override
+    public void onListeningStarted() {}
+
+    @Override
+    public void onListeningCanceled() {}
+
+    @Override
+    public void onListeningFinished() {}
+
+    @Override
+    public void onAudioLevel(final float level) {}
 
 }
