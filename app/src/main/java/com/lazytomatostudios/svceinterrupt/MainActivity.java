f240dd7faa6caad23815d9652f15219cb69e8b88
Jashaul <jashaulkumar@yahoo.com>
2017-08-12 14:36:17 +0530

    jashaul test 0

--- app/src/main/java/com/lazytomatostudios/svceinterrupt/MainActivity.java
+++ app/src/main/java/com/lazytomatostudios/svceinterrupt/MainActivity.java
@@ -37,9 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initNavBar();
-
     }
 
     public void initNavBar() {
